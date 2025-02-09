a083d7756eef4c8be3921f2958fb3befa4af9afb
benpeli <benjaminpellegrini@gmail.com>
2025-02-08 23:57:37 -0500

    just in case

---
+++ app/context/UserContext.tsx
@@ -0,0 +1,38 @@
+import React, { createContext, useContext, useState } from "react";
+
+// Define the user type
+interface User {
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+}
+
+// Define the context type
+interface UserContextType {
+  user: User | null;
+  setUser: (user: User | null) => void;
+}
+
+// Create the context
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+// Create a custom hook to use the context
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUser must be used within a UserProvider");
+  }
+  return context;
+};
+
+// Create the context provider
+export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,29 +18,40 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Loader2 } from 'lucide-react'
 
 const formSchema = z.object({
-    username: z.string().min(4, {
-      message: "Username must be at least 4 characters.",
-    }),
-  })
-
+  // Sign-Up
+  firstName: z.string().min(1, { message: "First name is required" }),
+  lastName: z.string().min(1, { message: "Last name is required" }),
+  // Sign-In
+  email: z.string().email().min(1, { message: "Email is required" }),
+  username: z.string().min(3, { message: "Username must be at least 3 characters" }),
+  password: z.string().min(8, { message: "Password must be at least 8 characters" }),
+});
 
 const AuthForm = ({ type }: {type: string}) => {
-    const [user, setuser] = useState(null);
+    const [user, setUser] = useState<null | { firstName: string; lastName: string }>(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     //Define form
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
       username: "",
+      password: ""
     },
   })
  
   //Define a submit handler
   function onSubmit(values: z.infer<typeof formSchema>) {
     //Do something with the form values
+    setIsLoading(true)
     console.log(values)
+    setIsLoading(false)
   }
 
   return (
@@ -82,25 +93,141 @@ const AuthForm = ({ type }: {type: string}) => {
             <>
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <>
+                        <FormField
+                        control={form.control}
+                        name="firstName"
+                        render={({ field }) => (
+                          <div className='form-item'>
+                            <FormLabel className='form-label'>
+                              First Name
+                            </FormLabel>
+                            <div className='flex w-full flex-col'>
+                              <FormControl>
+                                <Input 
+                                  placeholder="ex: John"
+                                  className='input-class'
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage className='form-message mt-2' />
+                            </div>
+                          </div>
+                        )}
+                        />
+                        <FormField
+                        control={form.control}
+                        name="lastName"
+                        render={({ field }) => (
+                          <div className='form-item'>
+                            <FormLabel className='form-label'>
+                              Last Name
+                            </FormLabel>
+                            <div className='flex w-full flex-col'>
+                              <FormControl>
+                                <Input 
+                                  placeholder="ex: Doe"
+                                  className='input-class'
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage className='form-message mt-2' />
+                            </div>
+                          </div>
+                        )}
+                        />
+
+                        </>
+                        <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                          <div className='form-item'>
+                            <FormLabel className='form-label'>
+                              Email
+                            </FormLabel>
+                            <div className='flex w-full flex-col'>
+                              <FormControl>
+                                <Input 
+                                  placeholder="Enter your email"
+                                  className='input-class'
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage className='form-message mt-2' />
+                            </div>
+                          </div>
+                        )}
+                        />
                         <FormField
                         control={form.control}
                         name="username"
                         render={({ field }) => (
-                            <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                            </FormDescription>
-                            <FormMessage />
-                            </FormItem>
+                          <div className='form-item'>
+                            <FormLabel className='form-label'>
+                              Username
+                            </FormLabel>
+                            <div className='flex w-full flex-col'>
+                              <FormControl>
+                                <Input 
+                                  placeholder="Enter your username"
+                                  className='input-class'
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage className='form-message mt-2' />
+                            </div>
+                          </div>
                         )}
                         />
-                        <Button type="submit">Submit</Button>
+                        <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                          <div className='form-item'>
+                            <FormLabel className='form-label'>
+                              Password
+                            </FormLabel>
+                            <div className='flex w-full flex-col'>
+                              <FormControl>
+                                <Input 
+                                  placeholder="Enter your password"
+                                  className='input-class'
+                                  type='password'
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage className='form-message mt-2' />
+                            </div>
+                          </div>
+                        )}
+                        />
+                      <div className='flex flex-col gap-4'>
+                        <Button type="submit" disabled={isLoading}
+                        className='form-btn'>
+                          {isLoading ? (
+                            <>
+                              <Loader2 size={20}
+                              className='animate-spin' /> Loading...
+                            </>
+                          ) : type === 'sign-in'
+                            ? 'Sign In' : 'Sign Up'}
+                        </Button>
+                        </div>
                     </form>
                 </Form>
+                
+                <footer className='flex justify-center gap-1'>
+                  <p className='text-14 font-normal text-gray-600'>
+                    {type === 'sign-in'
+                    ? "Don't have an account? "
+                    : "Already have an account? "}
+                  </p>
+                  <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className='form-link'>
+                    {type === 'sign-in' ? 'Sign Up' : 'Sign In'}
+                  </Link>
+                </footer>
+
             </>
         )}
     </section>
